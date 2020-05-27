86715d116c976c0dcc89cafe2d01b07b6ac1fae4
Prabesh Thapa <prabesh.thapa@yourkarma.com>
2020-05-27 20:07:41 +1000

    Added templates and nav bar

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('linux/', views.linux, name='blog-linux'),
+    path('security/', views.security, name='blog-security'),
+    path('devops/', views.devops, name='blog-devops')
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author':'Prabesh Thapa',
+        'title':'Blog post 1',
+        'Content':'Content 1',
+        'date_posted':'May 22, 2020'
+     },
+     {
+        'author':'Prabesh Thapa',
+        'title':'Blog post 2',
+        'Content':'Content 2',
+        'date_posted':'May 22, 2020'
+    }
+
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+   context = {
+       'posts':posts
+   } 
+    # return HttpResponse('<h1>Blog Home</h1>')
+   return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h2>about</h2>')
+    # return HttpResponse('<h2>about</h2>')
+    return render(request, 'blog/about.html', {'title':'About'})
+
+def linux(request):
+    return render(request, 'blog/linux.html', {'title':'Linux'})
+
+def security(request):
+    return render(request, 'blog/security.html', {'title':'security'})
+
+def devops(request):
+    return render(request, 'blog/devops.html', {'title':'devops'})
