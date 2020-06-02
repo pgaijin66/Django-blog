1f52ea56a9c68d3dde1dd23898765f6d51d47033
Prabesh Thapa <pgaijin66@gmail.com>
2020-06-02 23:08:28 +1000

    Login and Logout added

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISYPY_TEMPALTE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL='blog-home'
+
+LOGIN_URL='login'
--- blogproject/users/views.py
+++ blogproject/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -9,9 +10,12 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} !')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been create ! You are able to log in.')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form })
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html',)
